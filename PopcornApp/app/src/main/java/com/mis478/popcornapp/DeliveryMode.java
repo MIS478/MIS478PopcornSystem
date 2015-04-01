fd802df2c0847de04e8988371767dfad27ba3716
Brandon Casey <bcasey266@gmail.com>
2015-03-31 22:51:27 -0500

    Brandon - Nightly Build
    Fixed some merging issues and added the screens that I will be working
    on.

--- PopcornApp/app/src/main/java/com/mis478/popcornapp/DeliveryMode.java
+++ PopcornApp/app/src/main/java/com/mis478/popcornapp/DeliveryMode.java
@@ -5,43 +5,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 
-public class ScoutGoal extends ActionBarActivity {
-    int PersonalGoalInt;
+public class DeliveryMode extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scout_goal);
+        setContentView(R.layout.activity_delivery_mode);
 
         //Get info from the previous screen
         Intent intent = getIntent();
-
-        //find the Scout's personal goal amount
-        TextView textView = (TextView) findViewById(R.id.PersonalAmt);
-        String message = textView.getText().toString();
-        PersonalGoalInt=Integer.parseInt(message.replaceAll("[\\D]", ""));
-
-        //find the total amount of goods sold
-        TextView textView2 = (TextView) findViewById(R.id.TotalSoldAmt);
-        String message2 = textView2.getText().toString();
-        int i=Integer.parseInt(message2.replaceAll("[\\D]", ""));
-
-        //find the progress bar and then set the progress amount
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.SaleProgressBar);
-        progressBar.setMax(PersonalGoalInt);
-        progressBar.setProgress(i);
-
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_scout_goal, menu);
+        getMenuInflater().inflate(R.menu.menu_delivery_mode, menu);
         return true;
     }
 
