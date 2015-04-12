f78c4740023728f2b13394b06a7cd27bd9026dd0
Lloyd Davis <ldavis@mail.bradley.edu>
2015-04-12 03:08:50 -0500

    Working Donation Page
    I got the donation page to be functional.

--- PopcornApp/app/src/main/java/com/mis478/popcornapp/Donation.java
+++ PopcornApp/app/src/main/java/com/mis478/popcornapp/Donation.java
@@ -5,9 +5,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class Donation extends ActionBarActivity {
+    public final static String test = "com.mis478.popcornapp.test";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +20,44 @@ public class Donation extends ActionBarActivity {
         Intent intent = getIntent();
     }
 
+    public void ScoutMain(View view) {
+        Intent intent = new Intent(this, ScoutMainPage.class);
+        startActivity(intent);
+    }
+
+    public void CustomerInfo(View view) {
+        Intent intent = new Intent(this, CustomerInfo.class);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_donation, menu);
         return true;
+    }
+
+    public void ToFinalize(View view) {
+        //when the "Next" button is hit it creates an intent
+        Intent finalize = new Intent(this, OrderFinalization.class);
+
+        //finds the EditText boxes on the screen that contain the Customer's info
+        EditText amount = (EditText) findViewById(R.id.editText17);
+
+        //parses the EditText into a string
+        String AmountText = amount.getText().toString();
+
+
+        //attaches the donation amount to the intent
+        finalize.putExtra(test, AmountText);
+
+        //starts the next screen
+        startActivity(finalize);
+    }
+
+    public void ToProducts(View view) {
+        Intent products = new Intent(this, OrderFinalization.class);
+        startActivity(products);
     }
 
     @Override
@@ -36,7 +71,7 @@ public class Donation extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
 }
