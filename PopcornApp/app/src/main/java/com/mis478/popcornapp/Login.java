0e79ace902a0f133450cb922a34de6c70c65c862
joecuster <jpipis@mail.bradley.edu>
2015-04-20 23:26:19 -0500

    Server input basic

--- PopcornApp/app/src/main/java/com/mis478/popcornapp/Login.java
+++ PopcornApp/app/src/main/java/com/mis478/popcornapp/Login.java
@@ -2,25 +2,92 @@ package com.mis478.popcornapp;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.*;
+import javax.sql.*;
+import java.*;
+import java.util.ArrayList;
+
+import javax.*;
+
 public class Login extends ActionBarActivity {
-   // public final static String EXTRA_MESSAGE = "com.mis478.popcornapp.MESSAGE";
+
     Integer x = 1;
     Integer y = 1;
+    public static final String KEY_121 = "http://207.179.202.218:1515/joe/1.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTitle("Login");
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
+        StrictMode.setThreadPolicy(policy);
     }
+    public void ConnectToDatabase(View View){
+        InputStream is = null;
 
+        String result = "";
+        //the year data to send
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("year","420"));
+
+
+        //http post
+        try{
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(KEY_121);
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+
+
+        }catch(Exception e){
+            Log.e("log_tag", "Error in http connection " + e.toString());
+        }
+        //convert response to string
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            result=sb.toString();
+            EditText LoginEditText = (EditText) findViewById(R.id.LoginUsername);
+            LoginEditText.setText(result);
+        }catch(Exception e){
+            Log.e("log_tag", "Error converting result "+e.toString());
+        }
+        //parse json data
+
+
+}
     public void Login(View view) {
 
         EditText LoginEditText = (EditText) findViewById(R.id.LoginUsername);
