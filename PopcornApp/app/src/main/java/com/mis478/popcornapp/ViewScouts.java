50e701d8a53bad4e35bd04fbc464dc399439b10f
joecuster <jpipis@mail.bradley.edu>
2015-04-25 23:24:25 -0500

    readded menu changes

--- PopcornApp/app/src/main/java/com/mis478/popcornapp/ViewScouts.java
+++ PopcornApp/app/src/main/java/com/mis478/popcornapp/ViewScouts.java
@@ -141,7 +141,7 @@ public class ViewScouts extends ActionBarActivity
         }
 
         if (id == R.id.sales_mode) {
-            Intent intent = new Intent(this, SalesMode.class);
+            Intent intent = new Intent(this, SellProducts.class);
             startActivity(intent);
         }
         if (id == R.id.donation) {
