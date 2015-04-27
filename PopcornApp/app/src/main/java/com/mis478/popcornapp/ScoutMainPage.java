d9ff13974ac5c06f35d6a4280f1a6b7008494d2e
joecuster <jpipis@mail.bradley.edu>
2015-04-26 20:15:47 -0500

    Revert "Delivery Mode db done"
    This reverts commit 42d4370ad435496ada734d198b1726c1cc5c1a4d.

--- PopcornApp/app/src/main/java/com/mis478/popcornapp/ScoutMainPage.java
+++ PopcornApp/app/src/main/java/com/mis478/popcornapp/ScoutMainPage.java
@@ -36,15 +36,6 @@ public class ScoutMainPage extends ActionBarActivity {
     }
     public void Delivery(View view) {
         Intent x = new Intent(this, DeliveryMode.class);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            String test = extras.getString("string");
-            String value = extras.getString("name");
-            String[] alldata = extras.getStringArray("all");
-            x.putExtra("name", value);
-            x.putExtra("all", alldata);
-            x.putExtra("string",test);
-        }
         startActivity(x);
     }
     public void Status(View view) {
